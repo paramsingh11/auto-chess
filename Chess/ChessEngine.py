41c7c10b08b3e39cdbb9047e547215bbd4c4ed70
Param Singh <siinghparam11@gmail.com>
2024-07-12 15:23:17 +0100

    Chess Structure

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,60 @@
+"This class will be used for Storing info of current state of chess game."
+
+class GameState():
+    def __init__(self):
+        #a chess board with all the pieces with an empty space represented by "--"
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+        self.whiteToMove = True
+        self.moveLog = []
+    
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove
+
+class Move():
+
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                   "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "C": 2, "d": 3,
+                   "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+    
+    def getChessNotation(self):
+        return self.getrankFile(self.startRow, self.startCol) + self.getrankFile(self.endRow, self.endCol)
+
+    def getrankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
